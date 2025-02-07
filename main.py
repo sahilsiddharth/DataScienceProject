cae947e4a2a1d8e2a2c447a3ec9c3724af5e81d0
Siddharth Siddharth <siddharth.siddharth@thalesgroup.com>
2025-02-07 16:53:50 +0530

    Data ingestion stage completed

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.DataScience import logger
-
+from src.DataScience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 logger.info("Welocme to the Project")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.initiate_data_ingestion()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
