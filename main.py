59b3fb4ad52420afbc2e80de996b78f1a89f50d7
Siddharth Siddharth <siddharth.siddharth@thalesgroup.com>
2025-02-10 16:15:38 +0530

    Data Transformation completed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.DataScience import logger
 from src.DataScience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.DataScience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
-
+from src.DataScience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 logger.info("Welocme to the Project")
 
 STAGE_NAME = "Data Ingestion stage"
@@ -24,3 +24,14 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataTransformationTrainingPipeline()
+   data_validation.initiate_data_transformation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
--- src/DataScience/config/configuration.py
+++ src/DataScience/config/configuration.py
@@ -1,6 +1,6 @@
 from src.DataScience.constants import *
 from src.DataScience.utils.common import *
-from src.DataScience.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
+from src.DataScience.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig)
 
 class ConfigurationManager():
     def __init__(self, config_file_path=CONFIG_FILE_PATH, 
@@ -37,3 +37,16 @@ class ConfigurationManager():
 
         )
         return data_validation_config
+    
+    def get_data_transformation_config(self)-> DataTransformationConfig:
+        config=self.config.data_transformation
+       
+        create_directories([config.root_dir])
+
+        data_transformation_config=DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            
+
+        )
+        return data_transformation_config
